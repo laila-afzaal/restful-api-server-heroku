8fb936932a2b5168a0986d7fdfe22280d5fa043e
Laila Afzaal <lailaafzaal06@gmail.com>
2022-08-11 20:02:25 +0500

    my 1st commit

---
+++ server.mjs
@@ -0,0 +1,43 @@
+import express from 'express'
+import cors from 'cors'
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+let user = []
+
+app.post('/user', (req, res) => {
+
+  console.log(req.body)
+  user.push(req.body)
+  res.send(' User is created :) ')
+
+})
+
+app.get('/user', (req, res) => {
+ 
+  res.send('here is your user: user1')
+})
+
+app.put('/user', (req, res) => {
+ 
+  res.send('user is modified')
+
+})
+
+app.delete('/user', (req, res) => {
+ 
+  res.send('here is your user: user1')
+
+})
+
+app.get('/', (req, res) => {
+  console.log('server has received a request! ');
+  res.send('Hello World :) !')
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
